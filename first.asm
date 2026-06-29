625fae4b270585f0a9d64f1a8ca6ccb4e5b65a54
Hazar2026 <mzaman@student.sdccd.edu>
2026-06-28 23:03:23 -0700

    Remove commented lines from first.asm

--- first.asm
+++ first.asm
@@ -2,17 +2,15 @@ section .text
     global _start
 
 _start:
-    ; print the message
-    mov eax, 4          ; sys_write
-    mov ebx, 1          ; stdout
-    mov ecx, msg        ; message address
-    mov edx, len        ; message length
-    int 0x80            ; call kernel
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg
+    mov edx, len
+    int 0x80
 
-    ; exit the program
-    mov eax, 1          ; sys_exit
-    mov ebx, 0          ; exit code 0
-    int 0x80            ; call kernel
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
 
 section .data
 msg db 'I came,', 0xA, 'I saw,', 0xA, 'I conquered.', 0xA
